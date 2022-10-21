a99391f9b2e78fa6ff549589723eb62994d00a03
Agoitz2 <93652574+Agoitz2@users.noreply.github.com>
2022-10-21 09:27:53 +0200

    Correccion errores, crear validacion de nombre
    y tambien crear una pagina index que vaya a guardar el nombre en una cookie

---
+++ form.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+
+<?php 
+include("concerts.php");
+/// AÑADE EL CODIGO NECESARIO PARA PODER ACCEDER AL CONTENIDO DEL FICHERO concerts.php
+
+?>
+
+</head>
+
+
+<body>
+
+    <!-- <td>
+        <th>Nombre del artista</th>
+        <th>Ciudad</th>
+        <th>Estilo</th>
+        <th>Fecha</th>
+        <th>Asistencia</th>
+        <th>Ingresos</th>
+    </td>
+    <td>
+        <tr></tr>
+    </td> -->
+    <p><?php echo $_COOKIE["nombre"]; ?></p>
+
+    <form action="form.php" type="post">
+        <p>Nombre del artista</p>
+        <input type="text" name="nombre">
+        <p>Ciudad</p>
+        <input type="text" name="ciudad">
+        <p>Estilo</p>
+        <input type="text" name="estilo">
+        <p>Fecha</p>
+        <input type="text" name="fecha">
+        <p>Asistencia</p>
+        <input type="text" name="asistencia">
+        <p>Ingresos</p>
+        <input type="text" name="ingresos">
+        <br><br>
+        <input type="submit" name="submit" value="Enviar">
+    </form>
+
+
+<?php
+
+    if ($_POST('nombre').lenght >= 2 && $_POST('nombre').lenght <= 15) {
+
+        $db = new DBManager();
+        //comprueba que se haya iniciado el Post y si se ha iniciado va a enviar los datos a la base de datos
+        if (isset($_POST)) {
+            $nom = htmlentities($_POST("nombre"));
+            $ciu = htmlentities($_POST("ciudad"));
+            $est = htmlentities($_POST("estilo"));
+            $fec = htmlentities($_POST("fecha"));
+            $asi = htmlentities($_POST("asistencia"));
+            $ing = htmlentities($_POST("ingresos"));
+            $concert = new Concert("$nom", "$ciu", "$est", "$fec", $asi, $ing);
+            $db->storeConcert($concert);
+        }
+
+    }
+        
+    
+    // EJEMPLO:
+    // $db = new DBManager();
+    
+    // $concert = new Concert("Foo Fighters", "London", "Rock", date("Y-m-d", strtotime("2020-03-01")), 85000, 1000000);
+    // $db->storeConcert($concert);
+
+    // $concert = new Concert("Harry Styles", "New York", "Pop", date("Y-m-d", strtotime("2022-05-05")), 20000, 500000);
+    // $db->storeConcert($concert);
+
+    $concertsArray = $db->getConcerts();
+
+    echo"<ul>";
+    foreach ($concertsArray as $concert){
+        echo "<li>$concert</li>";
+    }
+    echo "<ul>";
+    
+
+?>
+
+</body>
+
+</html>
